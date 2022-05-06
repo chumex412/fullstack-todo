eeeaaca7d3a8018e82818ea15942b93ebd739d94
chumex412 <64347090+chumex412@users.noreply.github.com>
2022-05-06 17:23:37 +0100

    added the different routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import GlobablStyle from './components/GlobalStyles';
+import Nav from './components/Nav/index';
+import Home from './pages/Home';
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
-    <div>App</div>
+    <div>
+      <GlobablStyle />
+      <Nav />
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
+      </Routes>
+    </div>
   )
 }
 
