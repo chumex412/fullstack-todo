ae308a2f197f6fd59629aca2499dde5382943061
chumex412 <64347090+chumex412@users.noreply.github.com>
2022-05-08 22:18:05 +0100

    Added a strict mode check and wrapped the app with the redux provider

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,19 @@
-import React from "react";
+import React, {StrictMode} from "react";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import store from './store';
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
 const root = createRoot(document.getElementById("root"))
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </StrictMode>
 );
 
 
