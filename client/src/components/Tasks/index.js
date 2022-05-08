42113efec10fd3c933ed8cbbdba064dde405e22c
chumex412 <64347090+chumex412@users.noreply.github.com>
2022-05-08 22:11:58 +0100

    task list component

--- client/src/components/Tasks/index.js
+++ client/src/components/Tasks/index.js
@@ -1,26 +1,89 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styled from 'styled-components';
 
-const Tasks = ({tasks}) => {
+import TaskItem from './TaskItem';
+
+import EditFormModal from '../Form/EditFormModal';
+import { useUpdateTaskMutation, useRemoveTaskMutation } from '../../_services/taskServices';
+
+const Tasks = ({tasks, userId}) => {
+  const [updateTask] = useUpdateTaskMutation();
+  const [removeTask] = useRemoveTaskMutation();
+
+  const [today, setToday] = useState([]);
+  const [overdue, setOverdue] = useState([]);
+  const [show, setShow] = useState(false);
+  const [itemId, setItemId] = useState('');
+
+  const showModal = (id) => {
+    setShow(true)
+    setItemId(id)
+  }
+
+  const closeModal = () => {
+    setShow(false)
+  }
+
+  // Handler to complete a task
+  const completeTask = (id) => {
+    const completedTask = tasks.map(task => {
+      if(id === task._id) {
+        return { ...task, completed: !task.completed }
+      }
+
+      return task
+    })
+
+    updateTask({
+      id: userId,
+      tasks: completedTask
+    })
+  }
+
+  // Handler to remove a task
+  const removeTaskHandler = (id) => {
+    const filteredTask = tasks.filter(task => task._id !== id)
+
+    removeTask({
+      id: userId,
+      tasks: filteredTask
+    })
+  }
 
   return (
-    <section>
+    <TaskList>
+      <h3>Today's tasks</h3>
       {
         tasks.map(task => {
-          const { completed, name, id } = task;
+          const { completed, name, _id } = task;
           return (
-            <article key={id}>
-              <label>{name}</label>
-              <input
-                type="checkbox" 
-                value={completed}
-                onChange={() => {}}
-              />
-            </article>
+            <TaskItem 
+              key={_id}
+              id={_id}
+              name={name}
+              completed={completed}
+              completeTask={completeTask}
+              remove={removeTaskHandler}
+              showModal={showModal}
+            />
           )
         })
       }
-    </section>
+      <EditFormModal 
+        show={show}
+        close={closeModal}
+        tasks={tasks}
+        userId={userId}
+        id={itemId}
+      />
+    </TaskList>
   )
 }
+
+const TaskList = styled.section`
+  padding: 1.5rem;
+  display: grid;
+  gap: 2rem;
+`;
 
 export default Tasks
