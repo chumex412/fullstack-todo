ba034c924746adff0e254f51edf7a164bf11b24f
chumex412 <64347090+chumex412@users.noreply.github.com>
2022-05-10 11:16:50 +0100

    wrapped app with context api

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,20 @@
+import 'whatwg-fetch';
 import React, {StrictMode} from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import store from './store';
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { AppProvider } from "./custom/Context";
 
 const root = createRoot(document.getElementById("root"))
 root.render(
   <StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-        <App />
+        <AppProvider>
+          <App />
+        </AppProvider>
       </Provider>
     </BrowserRouter>
   </StrictMode>
