9252837820bd6b1b23640d08ef9f9480b64151e4
chumex412 <64347090+chumex412@users.noreply.github.com>
2022-05-10 10:59:35 +0100

    imported alert state

--- client/src/_reducers/rootReducer.js
+++ client/src/_reducers/rootReducer.js
@@ -2,10 +2,12 @@ import { combineReducers } from "@reduxjs/toolkit";
 import registerReducer from './registerReducer';
 import loginReducer from './loginReducer';
 import { taskServices } from "../_services/taskServices";
+import alerReducer from "./alertReducer";
 
 const rootReducer = combineReducers({ 
   authentication: loginReducer,
   register: registerReducer,
+  alert: alerReducer,
   [taskServices.reducerPath]: taskServices.reducer
 })
 
